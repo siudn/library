d7f61d1346e381772d8f2705fe551f2c3d7cbe4f
siudn <cshadx@gmail.com>
2023-10-05 03:37:54 -0400

    Modal dialog pops up on button click

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+const myLibrary = [];
+const newBookDialog = document.querySelector("dialog")
+const newBookButton = document.getElementById("newBookButton")
+const closeButton = document.querySelector("dialog button")
+
+newBookButton.addEventListener("click", () => newBookDialog.showModal())
+closeButton.addEventListener("click", () => newBookDialog.close())
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
     this.info = function() {
-        if (read)
-            return `${title} by ${author}, ${pages} pages, read`;
+        if (read) return `${title} by ${author}, ${pages} pages, read`
         return `${title} by ${author}, ${pages} pages, not read yet`
     }
+}
+
+function addBookToLibrary(title, author, pages, read = false) {
+    title = Book(title, author, pages, read)
+    myLibrary.push(title)
+}
+
+function display() {
+
+}
+
+function newBook() {
+
 }
